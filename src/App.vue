e736f93e96c9131747a1d50f35e438384a1bf767
pvlasak <petr.vlasakk@gmail.com>
2026-02-27 18:21:10 +0100

    two way binding is implemented by using of v-model and methods in Vuejs also got simplified.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-        <div id="display-view">
+        <div v-show="!isEditMode">
             <h1> User profile </h1>
             <img :src="image">
 
@@ -14,20 +14,20 @@
             <button @click="handleEditProfile"> Edit Profile </button>
         </div>
 
-        <div id="edit-view">
+        <div v-show="isEditMode">
             <h1> User profile </h1>
             <img src="image">
 
             <span> Name: </span>
-            <input type="text" id="input-name" :placeholder="name" /> 
+            <input type="text" id="input-name" v-model="name" /> 
             <hr/>
             
             <span> Email: </span>
-            <input type="text" id="input-email" :placeholder="email" />
+            <input type="text" id="input-email" v-model="email" />
             <hr/> 
 
             <span> Interesets: </span> 
-            <input type="text" id="input-interests" :placeholder= "interests"/>
+            <input type="text" id="input-interests" v-model= "interests"/>
             <hr/>  
             <button @click="handleUpdateProfile"> Update Profile </button>            
          </div>
@@ -42,40 +42,16 @@ export default {
             image: image,
             name: "Anna Smith",
             email: "anna.smith@example.com",
-            interests: "coding"
+            interests: "coding",
+            isEditMode: false
         }
     },
     methods: {
-        handleEditProfile() {
-            var name=document.getElementById("name").textContent
-            var inputName=document.getElementById("input-name")
-            inputName.value=name
-
-            var email=document.getElementById("email").textContent
-            var inputEmail=document.getElementById("input-email")
-            inputEmail.value=email
-
-            var interests=document.getElementById("interests").textContent
-            var inputInterests=document.getElementById("input-interests")
-            inputInterests.value=interests
-                        
-            document.getElementById("edit-view").style.display="block";
-            document.getElementById("display-view").style.display="none"
+        handleEditProfile() {    
+            this.isEditMode = true                    
         },
         handleUpdateProfile() {
-            var updated_name = document.getElementById("input-name").value
-            var name=document.getElementById("name")
-            name.textContent = updated_name
-
-            var updated_email = document.getElementById("input-email").value
-            var email=document.getElementById("email")
-            email.textContent = updated_email
-
-            var updated_interests = document.getElementById("input-interests").value
-            var interests=document.getElementById("interests")
-            interests.textContent = updated_interests
-            document.getElementById("edit-view").style.display="none";
-            document.getElementById("display-view").style.display="block"
+            this.isEditMode = false
         }
     }
 }
@@ -113,10 +89,6 @@ input {
     width: 200px;
     font-size: 15px;
     padding: 10px;
-}
-
-#edit-view {
-    display: none;
 }
 
 .datepicker {
