20ca169de927abbd1e8d10d0661ebb1dd11b2c0d
pvlasak <petr.vlasakk@gmail.com>
2026-03-03 17:26:50 +0100

    jest test file validator.test.js to test invalid and valid email is added

---
+++ validator.test.js
@@ -0,0 +1,31 @@
+function isInvalidEmail(userObject) {
+    return !userObject.email.includes("@")
+}
+
+function isEmptyPayload(userObject) {
+    return Object.keys(userObject).length === 0
+}
+module.exports = {
+    isInvalidEmail,
+    isEmptyPayload
+}
+
+test('valid email', function() {
+    const testPayload = {
+        name: "test name",
+        email: "test.email@example.com",
+        interests: "coding"
+    }
+    const result = isInvalidEmail(testPayload)
+    expect(result).toBe(false)
+})
+
+test('invalid email', function() {
+    const testPayload = {
+        name: "test name",
+        email: "test.email",
+        interests: "coding"
+    }
+    const result = isInvalidEmail(testPayload)
+    expect(result).toBe(true)
+})
